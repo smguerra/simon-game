fb9a52e97e4cbfddd1410b5c77980c9c97fba2a4
Milena <milenagg77@hotmail.com>
2023-01-30 11:55:39 +0000

    added function next sequence to project

---
+++ assets/script/index.js
@@ -0,0 +1,13 @@
+let buttonColors = ["red", "blue", "green", "yellow"];
+let gamePattern = [];
+
+
+function nextSequence() {
+   let randomNumber = Math.floor(Math.random() * 4);
+   let randomChosenColor = buttonColors[randomNumber];
+   gamePattern.push(randomChosenColor);
+
+}
+
+
+
